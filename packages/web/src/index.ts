f3b451eb7b61c403b645de43b5e79b8adb2040d9
Alexander Lobyntsev <yakutoc@gmail.com>
2023-07-12 13:15:26 +0700

    feat(web): ref

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,21 +1,5 @@
-export const helloWorld = () => {
+export const callMeApi = () => {
 	console.log('Hello world')
-}
-
-export const newApiMethod = () => {
-	console.log('New API method');
-}
-
-export const newApiMethod2 = () => {
-	console.log('New API method 2');
-}
-
-export const newApiMethod3 = () => {
-	console.log('New API method 3');
-}
-
-export const newApiMethod4 = () => {
-	console.log('New API method 4');
 }
 
 
