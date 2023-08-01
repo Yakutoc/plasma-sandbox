bf55dbcafa83ac6ebde9db988aa76320c6b5a135
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-01 18:01:19 +0700

    feat: add

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,23 +1,3 @@
-export const otherMethod = () => {
-	console.log('Hello world');
-}
-
 export const otherMethod2 = () => {
-	console.log('Hello world');
-}
-
-export const otherMethod3 = () => {
-	console.log('Hello world');
-}
-
-export const otherMethod4 = () => {
-	console.log('Hello world');
-}
-
-export const otherMethod5 = () => {
-	console.log('Hello world');
-}
-
-export const otherMethod6 = () => {
 	console.log('Hello world');
 }
