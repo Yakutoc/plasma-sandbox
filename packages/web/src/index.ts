3dfe184e578ec2347ac1ff7a342b6c7ea4e74d8d
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-07 17:17:45 +0700

    feat(web): add new method

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -2,26 +2,10 @@ export const otherMethod2 = () => {
 	console.log('Hello world');
 }
 
-export const method = () => {
-	console.log('Hello world');
-}
-
-export const call = () => {
-	console.log('Hello world');
-}
-
-export const other = () => {
-	console.log('Hello world');
-}
-
 export const callMe = () => {
 	console.log('Hello world');
 }
 
-export const callMeNow = () => {
-	console.log('Hello world');
-}
-
-export const callMeNow2 = () => {
+export const callMe2 = () => {
 	console.log('Hello world');
 }
