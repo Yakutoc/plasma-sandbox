0455cdf38d1ba679665fb4d4dd98eef2f3d0b681
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-08 16:54:15 +0700

    feat(hope): [skip ci]

---
+++ packages/hope/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var plasmax_core_1 = require("plasmax-core");
+plasmax_core_1.APICall();
+//# sourceMappingURL=index.js.map
