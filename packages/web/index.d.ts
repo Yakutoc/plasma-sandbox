322cf7aeafc460e9fc1211554e446a0c91db91c7
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-08 14:38:59 +0700

    chore: [skip ci]

--- packages/web/index.d.ts
+++ packages/web/index.d.ts
@@ -4,3 +4,4 @@ export declare const newAPICall: () => void;
 export declare const newAPICall2: () => void;
 export declare const newAPICall3: () => void;
 export declare const newAPICall4: () => void;
+export declare const newAPICall5: () => void;
--- packages/web/index.js
+++ packages/web/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.newAPICall4 = exports.newAPICall3 = exports.newAPICall2 = exports.newAPICall = exports.callMe = exports.otherMethod2 = void 0;
+exports.newAPICall5 = exports.newAPICall4 = exports.newAPICall3 = exports.newAPICall2 = exports.newAPICall = exports.callMe = exports.otherMethod2 = void 0;
 exports.otherMethod2 = function () {
     console.log('Hello world');
 };
@@ -17,6 +17,9 @@ exports.newAPICall3 = function () {
     console.log('Hello world');
 };
 exports.newAPICall4 = function () {
+    console.log('Hello world');
+};
+exports.newAPICall5 = function () {
     console.log('Hello world');
 };
 //# sourceMappingURL=index.js.map
