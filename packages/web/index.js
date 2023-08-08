5af62b979c00d807aedd7a0f0ab2ffda10ab93f0
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-08 14:33:58 +0700

    chore: [skip ci]

---
+++ packages/web/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.newAPICall4 = exports.newAPICall3 = exports.newAPICall2 = exports.newAPICall = exports.callMe = exports.otherMethod2 = void 0;
+exports.otherMethod2 = function () {
+    console.log('Hello world');
+};
+exports.callMe = function () {
+    console.log('Hello world');
+};
+exports.newAPICall = function () {
+    console.log('Hello world');
+};
+exports.newAPICall2 = function () {
+    console.log('Hello world');
+};
+exports.newAPICall3 = function () {
+    console.log('Hello world');
+};
+exports.newAPICall4 = function () {
+    console.log('Hello world');
+};
+//# sourceMappingURL=index.js.map
