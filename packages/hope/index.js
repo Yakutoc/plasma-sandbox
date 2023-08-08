8a14c1e8463871f53e534854600b4d6c27d39514
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-08 17:07:15 +0700

    feat(hope): add api

--- packages/hope/index.js
+++ packages/hope/index.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.APICall = void 0;
 var plasmax_core_1 = require("plasmax-core");
+var plasmax_core_2 = require("plasmax-core");
+Object.defineProperty(exports, "APICall", { enumerable: true, get: function () { return plasmax_core_2.APICall; } });
 plasmax_core_1.APICall();
 //# sourceMappingURL=index.js.map
