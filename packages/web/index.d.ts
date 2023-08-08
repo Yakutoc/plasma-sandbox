a22ce1017f90567b9b755b396c9a60e0da071c23
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-08 14:29:10 +0700

    chore: [skip ci]

---
+++ packages/web/index.d.ts
@@ -0,0 +1,6 @@
+export declare const otherMethod2: () => void;
+export declare const callMe: () => void;
+export declare const newAPICall: () => void;
+export declare const newAPICall2: () => void;
+export declare const newAPICall3: () => void;
+export declare const newAPICall4: () => void;
