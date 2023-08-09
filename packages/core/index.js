a8820577cc15c60b7461d4c58333d16761c5bc8c
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-09 09:50:45 +0700

    feat(core): add new method

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.APICall = void 0;
+exports.APICall2 = exports.APICall = void 0;
 exports.APICall = function () {
+    console.log('API Call');
+};
+exports.APICall2 = function () {
     console.log('API Call');
 };
 //# sourceMappingURL=index.js.map
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
 export const APICall = () => {
 	console.log('API Call');
 }
+
+export const APICall2 = () => {
+	console.log('API Call');
+}
