9cb31ab588b9c6594774cea04d8c8f032d560c79
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-17 16:40:26 +0700

    feat(b2c): other test commit 2

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.APICall2 = exports.APICall = void 0;
+exports.APICall3 = exports.APICall2 = exports.APICall = void 0;
 exports.APICall = function () {
     console.log('API Call');
 };
 exports.APICall2 = function () {
+    console.log('API Call');
+};
+exports.APICall3 = function () {
     console.log('API Call');
 };
 //# sourceMappingURL=index.js.map
