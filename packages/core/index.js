315cdd48f23268b992723498ce4048b647a8bb85
Alexander Lobyntsev <yakutoc@gmail.com>
2023-08-29 18:40:39 +0700

    feat: new api

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.APICall4 = exports.APICall3 = exports.APICall2 = exports.APICall = void 0;
+exports.APICall5 = exports.APICall4 = exports.APICall3 = exports.APICall2 = exports.APICall = void 0;
 exports.APICall = function () {
     console.log('API Call');
 };
@@ -11,6 +11,9 @@ exports.APICall3 = function () {
     console.log('API Call');
 };
 exports.APICall4 = function () {
+    console.log('API Call');
+};
+exports.APICall5 = function () {
     console.log('API Call');
 };
 //# sourceMappingURL=index.js.map
