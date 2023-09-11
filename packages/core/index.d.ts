06eb3335399a1dc57188b80abf03d9e184963ad7
Alexander Lobyntsev <yakutoc@gmail.com>
2023-09-11 19:19:03 +0700

    feat: new api

--- packages/core/index.d.ts
+++ packages/core/index.d.ts
@@ -3,3 +3,4 @@ export declare const APICall2: () => void;
 export declare const APICall3: () => void;
 export declare const APICall4: () => void;
 export declare const APICall5: () => void;
+export declare const APICall6: () => void;
