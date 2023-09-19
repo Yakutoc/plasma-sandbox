87a1dc68123eaa79c2878f9ef2ee86a799b65d52
Alexander Lobyntsev <yakutoc@gmail.com>
2023-09-19 13:00:54 +0700

    feat: after this commit bump version for all packs

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.APICall8 = exports.APICall7 = exports.APICall6 = exports.APICall5 = exports.APICall4 = exports.APICall3 = exports.APICall2 = exports.APICall = void 0;
+exports.APICall9 = exports.APICall8 = exports.APICall7 = exports.APICall6 = exports.APICall5 = exports.APICall4 = exports.APICall3 = exports.APICall2 = exports.APICall = void 0;
 exports.APICall = function () {
     console.log('API Call');
 };
@@ -24,6 +24,9 @@ exports.APICall7 = function () {
     console.log('API Call 87');
 };
 exports.APICall8 = function () {
+    console.log('API Call 87');
+};
+exports.APICall9 = function () {
     console.log('API Call 87');
 };
 //# sourceMappingURL=index.js.map
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -31,3 +31,7 @@ export const APICall8 = () => {
 	console.log('API Call 87');
 }
 
+export const APICall9 = () => {
+	console.log('API Call 87');
+}
+
