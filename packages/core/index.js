658d125fbb2b025d76ee17781e7326d2aa0d3f04
Alexander Lobyntsev <yakutoc@gmail.com>
2023-10-09 12:30:21 +0700

    feat: change by fork

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,33 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.APICall8 = exports.APICall7 = exports.APICall6 = exports.APICall5 = exports.APICall4 = exports.APICall3 = exports.APICall2 = exports.APICall = void 0;
-exports.APICall = function () {
+var APICall = function () {
     console.log('API Call');
 };
-exports.APICall2 = function () {
+exports.APICall = APICall;
+var APICall2 = function () {
     console.log('API Call');
 };
-exports.APICall3 = function () {
+exports.APICall2 = APICall2;
+var APICall3 = function () {
     console.log('API Call');
 };
-exports.APICall4 = function () {
+exports.APICall3 = APICall3;
+var APICall4 = function () {
     console.log('API Call');
 };
-exports.APICall5 = function () {
+exports.APICall4 = APICall4;
+var APICall5 = function () {
     console.log('API Call');
     console.log('API Call');
     console.log('API Call');
-    console.log('API Call');
-    console.log('API Call');
-};
-exports.APICall6 = function () {
     console.log('API Call');
     console.log('API Call');
 };
-exports.APICall7 = function () {
+exports.APICall5 = APICall5;
+var APICall6 = function () {
+    console.log('API Call');
     console.log('API Call');
 };
-exports.APICall8 = function () {
+exports.APICall6 = APICall6;
+var APICall7 = function () {
     console.log('API Call');
 };
+exports.APICall7 = APICall7;
+var APICall8 = function () {
+    console.log('API Call + whatt!!!');
+};
+exports.APICall8 = APICall8;
 //# sourceMappingURL=index.js.map
