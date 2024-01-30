b859bed62d7e5d2471d37e136e4dc9ea022fdd97
Alexander Lobyntsev <yakutoc@gmail.com>
2024-01-30 21:22:04 +0700

    ci: 8-14 [no ci]

--- .github/read.js
+++ .github/read.js
@@ -2,10 +2,12 @@ module.exports = () => {
 	const { PACKAGES_STORYBOOK, PACKAGES } = process.env;
 	
 	const packages = JSON.parse(JSON.parse(PACKAGES));
-	const packages_storybook = JSON.parse(PACKAGES_STORYBOOK);
+	const packages_storybook = JSON.parse(JSON.parse(PACKAGES_STORYBOOK));
 	
 	console.log('packages ==> ', packages);
 	console.log('PACKAGES_STORYBOOK ==> ', packages_storybook);
 	
-	return {}
+	return {
+		'some': true
+	}
 }
