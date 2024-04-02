4c54bb8d1f6aa0a17ddf07b72bcfad11921ef6ea
Alexander Lobyntsev <yakutoc@gmail.com>
2024-04-02 17:06:15 +0700

    ci: update settings [no ci]

--- auto.config.js
+++ auto.config.js
@@ -18,10 +18,8 @@ const uploadAssetsPluginOptions = {
 
 /** Auto configuration */
 module.exports = function rc() {
-    const { upload_assets: uploadAssets = 'false', packages_targets } = process.env || {};
+    const { UPLOAD_ASSETS: uploadAssets = 'false', packages_targets } = process.env || {};
     const plugins = [['released', releasedOptions], ['npm', npmOptions], 'conventional-commits'];
-    
-    console.log(packages_targets);
     
     if (uploadAssets === 'true') {
         plugins.unshift(['./auto-plugins/dist/upload-assets-extend.js',
